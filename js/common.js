2377c5cd7f8d2bedfc602b7a2a6ba0d3e0670813
Simmer <xushujun_sunny@163.com>
2017-01-03 19:20:15 +0800

    publish blog

--- js/common.js
+++ js/common.js
@@ -3,26 +3,26 @@ window.onload = function() {
     navBtn.click(function(){
         $('.nav').toggleClass('nav-show');
     })
-// go to top 
-        function goTop() {
-          $(window).scroll(function(e) {
-              // If the distance between scrollbar and top border is more than 100 pix
-              if($(window).scrollTop() > 800)
-                  $("#go-pg-top").fadeIn().css({ opacity: 0.6 });
-              else
-                  $("#go-pg-top").fadeOut();
-          });
-          };  
+		// go to top 
+    function goTop() {
+      $(window).scroll(function(e) {
+        // If the distance between scrollbar and top border is more than 100 pix
+        if($(window).scrollTop() > 800)
+          $("#go-pg-top").fadeIn().css({ opacity: 0.6 });
+        else
+          $("#go-pg-top").fadeOut();
+      });
+      };  
 
-          $("#go-pg-top").click(function(e) {
-              $('body,html').animate({scrollTop:0}, 200);
-          });
-          $("#go-pg-top").mouseover(function(e) {
-              $(this).css({ opacity: 1 });
-          });
-          $("#go-pg-top").mouseout(function(e) {
-              $(this).css({ opacity: 0.6 });
-          });
-          // Scroll to top
-          goTop();
+      $("#go-pg-top").click(function(e) {
+          $('body,html').animate({scrollTop:0}, 200);
+      });
+      $("#go-pg-top").mouseover(function(e) {
+          $(this).css({ opacity: 1 });
+      });
+      $("#go-pg-top").mouseout(function(e) {
+          $(this).css({ opacity: 0.6 });
+      });
+      // Scroll to top
+      goTop();
 }
